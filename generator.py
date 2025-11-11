58652f6cde02d47d0c6f79fee0984b7e2ddf827b
Soumil Rathi <soumilrathi@gmail.com>
2025-11-11 14:00:09 -0600

    Disable inductor cudagraphs for CSM-1B

--- generator.py
+++ generator.py
@@ -751,6 +751,7 @@ def load_csm_1b(device: str = "cuda") -> Generator:
     model = Model.from_pretrained("sesame/csm-1b")
     
     dtype = torch.bfloat16 if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else torch.float16
+    torch._inductor.config.triton.cudagraphs = False
     model.backbone = torch.compile(model.backbone,mode='reduce-overhead', fullgraph=True, backend='inductor')
     model.decoder = torch.compile(model.decoder,mode='reduce-overhead', fullgraph=True, backend='inductor')
 
